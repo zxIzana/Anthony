2b353a8c65ae1ca92a81dace0800c4c1c0d694bf
zxIzana <zxizanaa@gmail.com>
2024-10-20 13:36:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Teste", "🎮 Playing VALORANT"];
+const statusMessages = ["🐜 | Auxiliando no PYM!", "🐜 | A formiga mais linda desse servidor!"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
